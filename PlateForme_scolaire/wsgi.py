1654a49106d2f6496bf958d5f98782e453029af0
gloirekashala <gloirekashala879@gmail.com>
2025-05-07 02:42:55 -0700

    first commit

---
+++ PlateForme_scolaire/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for PlateForme_scolaire project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PlateForme_scolaire.settings')
+
+application = get_wsgi_application()
