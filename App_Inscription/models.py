ccff2b9b9d4806f2c1b95036f1c4d9cb1b86158e
gloirekashala <gloirekashala879@gmail.com>
2025-05-08 01:46:04 -0700

    ajout de mes applicatio

--- App_Inscription/models.py
+++ App_Inscription/models.py
@@ -2,43 +2,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.contrib.auth.validators import UnicodeUsernameValidator
 
-class User(AbstractUser):
-    STATUS = [("actif","actif"),("inactif","inactif")]
-    USER_TYPE = (
-        (0, 'administrateur'),
-        (1, 'promoteur'),
-        (2, 'prefet'),
-        (3, 'directeur'),
-        (4, 'comptable'),
-        (5, 'secretaire'),
-        (6, 'enseignant'),
-        (7, 'eleve'),
-    )
-    GENRE = [("M", "Masculin"), ("F", "Feminin")]
-    username = models.CharField(
-        max_length=150, 
-        validators=[UnicodeUsernameValidator, ], 
-        unique=True
-    )
-    email = models.EmailField(
-        unique=True, 
-        verbose_name='Email', 
-        max_length=255
-    )
-
-    nom = models.CharField(max_length=100)
-    postnom = models.CharField(max_length=100)
-    prenom = models.CharField(max_length=100)
-    adresse = models.TextField()
-    genre = models.CharField(max_length=1, choices=GENRE, null=True, blank=True)
-    photo = models.ImageField(upload_to='photos_users/', null=True, blank=True)
-    telephone = models.CharField(max_length=15, unique=True, null=True, blank=True)
-    
-    
-
-    def __str__(self):
-        return f"{self.username} ({self.prenom} {self.postnom})"
-    
+   
 
 class Eleve(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='eleve')
