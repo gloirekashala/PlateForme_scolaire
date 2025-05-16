13f8d4a036bcf762dbe74a63386bfe53ac9f6b18
Emmanuel Juif <juif_emmanuel@outlook.com>
2025-05-16 13:19:52 +0000

    Ajout du template

--- app_Comptable/urls.py
+++ app_Comptable/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import DashboardComptable, formulaire
 
 urlpatterns = [
-    path('', DashboardComptable.as_view(), name="dashboard"),
+    path('', DashboardComptable.as_view(), name="dashboard_comptable"),
     path('forms', formulaire, name="forms"),
 ]
--- app_User/views.py
+++ app_User/views.py
@@ -1,11 +1,29 @@
 from django.shortcuts import redirect, render
+from django.views import View
 from django.views.generic import TemplateView
 from app_User.models import User
+from django.contrib.auth import authenticate, login
+
 
 # Create your views here.
 
-class Login(TemplateView):
-    template_name = "user/login.html"
+class Login(View):
+    def get(self,request):
+        return render(request,'user/login.html')
+    
+    def post(self, request):
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        print("Où sont mes données :", username, password)
+        user = authenticate(request, username=username, password=password)
+        print("C est quoi avec toi: ", user)
+
+        if user is not None:
+            login(request, user)
+            print (login(request, user))
+            return redirect('dashboard_comptable')
+        else: 
+            return render(request, 'user/login.html', {'error': "Veuillez saisir les bonnes informations"})
 
 class Register(TemplateView):
     model = User
